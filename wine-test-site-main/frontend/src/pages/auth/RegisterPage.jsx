f8aae32abf73bbdcae2b6ffb8062b7c1f8b669bc
Mayur Khamker <mayurkhambkar@gmail.com>
2026-04-16 14:35:29 +0530

    name changes

--- wine-test-site-main/frontend/src/pages/auth/RegisterPage.jsx
+++ wine-test-site-main/frontend/src/pages/auth/RegisterPage.jsx
@@ -93,7 +93,7 @@ const RegisterPage = () => {
           value={formState.name}
           onChange={handleChange}
           error={errors.name}
-          placeholder="Mayur khamkar"
+          placeholder="type hear"
         />
 
         <FormField
